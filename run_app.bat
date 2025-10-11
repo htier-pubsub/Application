445570cdbe20c67566dcbe47522e5cb677ab4559
chindirala <chitti.chindirala@gmail.com>
2025-10-11 22:46:51 +0530

    Added RUST server executable for HTTP calls

--- run_app.bat
+++ run_app.bat
@@ -8,6 +8,10 @@ echo.
 
 python -m pip install --user streamlit plotly scikit-learn pandas numpy joblib paho-mqtt pymodbustcp requests streamlit-webrtc opencv-python av
 
+echo Make sure your rust-app.exe is running first!
+start ./RUST/debug/rust-app
+
+echo Running Streamlit App!
 start "Streamlit App" python -m streamlit run ./HtierApp/app.py --server.port 5001
 
 timeout /t 10 /nobreak
